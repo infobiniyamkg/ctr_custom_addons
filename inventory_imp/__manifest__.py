c7360fa90c54ab0b06cbaa8a6cddb77aedb8cdad
biniyamkg <biniyamkg@yahoo.com>
2026-08-07 11:27:10 +0300

    IMP pdf printout

---
+++ inventory_imp/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Inventory IMP Add",
+    "description": "Add Customs_ID  field to item transfers and delivery reports",
+    "depends": ["stock"],
+    "data": [
+        "views/moveView.xml",
+        "reports/inventory_report_extend.xml"
+    ],
+
+    "author": "Afro Dynamic",
+
+}
